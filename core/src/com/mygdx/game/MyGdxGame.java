13759160850a1185a56d78a2a7f4885be8d2b478
ShuLgaSeR-collab <shulgin.sery@yandex.ru>
2022-04-13 20:38:15 +0500

    Made joystick and simple character

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -9,10 +9,20 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import org.graalvm.compiler.replacements.Log;
@@ -24,9 +34,23 @@ public class MyGdxGame extends ApplicationAdapter  {
 
 
 	OrthographicCamera camera;
+	Touchpad touchpad;
 
 	SpriteBatch batch;
 	Sprite sprite;
+
+	Stage stage;
+	Skin touchpadSkin;
+
+	Touchpad.TouchpadStyle touchpadStyle;
+	float cameraMovementSpeed = .3f;
+
+	private Drawable touchBackground;
+	private Drawable touchKnob;
+
+	Texture blockTexture;
+	float blockSpeed;
+	Sprite blockSprite;
 
 	
 	@Override
@@ -44,6 +68,39 @@ public class MyGdxGame extends ApplicationAdapter  {
 		camera.update();
 
 		batch = new SpriteBatch();
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+
+		touchpadSkin = new Skin();
+		//Set background image
+		touchpadSkin.add("touchBackground", new Texture("trans.png"));
+		//Set knob image
+		touchpadSkin.add("touchKnob", new Texture("img_2.png"));
+		//Create TouchPad Style
+		touchpadStyle = new Touchpad.TouchpadStyle();
+		//Create Drawable's from TouchPad skin
+		touchBackground = touchpadSkin.getDrawable("touchBackground");
+		touchKnob = touchpadSkin.getDrawable("touchKnob");
+		//Apply the Drawables to the TouchPad Style
+		touchpadStyle.background = touchBackground;
+		touchpadStyle.knob = touchKnob;
+		//Create new TouchPad with the created style
+		touchpad = new Touchpad(10, touchpadStyle);
+		//setBounds(x,y,width,height)
+		touchpad.setBounds(15, 15, 200, 200);
+		//Create a Stage and add TouchPad
+		stage = new Stage(new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+		stage.addActor(touchpad);
+		Gdx.input.setInputProcessor(stage);
+
+		//Create block sprite
+		blockTexture = new Texture(Gdx.files.internal("img_3.png"));
+		blockSprite = new Sprite(blockTexture);
+		blockSprite.setSize(2, 2);
+		//Set position to centre of the screen
+		blockSprite.setPosition(camera.position.x, camera.position.y);
+
+		blockSpeed = .3f;
 	}
 
 	@Override
@@ -51,12 +108,17 @@ public class MyGdxGame extends ApplicationAdapter  {
 		handleInput();
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
-
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		blockSprite.setX(blockSprite.getX() + touchpad.getKnobPercentX() * blockSpeed);
+		blockSprite.setY(blockSprite.getY() + touchpad.getKnobPercentY() * blockSpeed);
 
 		batch.begin();
 		sprite.draw(batch);
+		blockSprite.draw(batch);
 		batch.end();
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
 
 	@Override
@@ -90,6 +152,7 @@ public class MyGdxGame extends ApplicationAdapter  {
 		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
 			camera.translate(0, .5f * camera.zoom, 0);
 		}
+		camera.translate(touchpad.getKnobPercentX() * cameraMovementSpeed * camera.zoom, touchpad.getKnobPercentY() * camera.zoom * cameraMovementSpeed, 0);
 		camera.zoom = MathUtils.clamp(camera.zoom, .5f, 1f);
 
 		float effectiveViewportWidth = camera.viewportWidth * camera.zoom;
